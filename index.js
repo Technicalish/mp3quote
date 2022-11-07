055b476f34669707675ddeab76a2380585efdbd7
Technicalish <66911671+Technicalish@users.noreply.github.com>
2022-11-07 16:37:18 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var express = require("express");
 var app = express();
+app.use("/", express.static("./public"), serveIndex("./public", { icons: true }));
 var serveIndex = require("serve-index");
 var fetch = (...args) => import("node-fetch").then(({ default: fetch }) => fetch(...args));
 var gTTS = require("gtts");
@@ -23,7 +24,5 @@ app.get("/custom.mp3", async (request, response) => {
   }
 });
 app.get("/favicon.png", (request, response) => response.sendFile(process.cwd() + "/favicon.png"));
-
-app.use(express.static("./public"), serveIndex("./public", { icons: true }));
 app.listen(process.env.PORT || 3000);
 module.exports = app;
